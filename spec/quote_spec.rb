fbda56d4985781e3e6bfded92ff2cbcccce59c87
Okikiola Apelehin <okikiola.apelehin@gmail.com>
2020-06-11 14:36:18 +0000

    Create the RSpec file Create the #quote_spec.rb file

---
+++ spec/quote_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../lib/quote.rb'
+
+describe 'Quote' do
+  describe '#create_request' do
+    let(:req) {Quote.new}
+    let(:req_json) { req.create_request }
+
+    it 'should return json response when the request is sucessful' do
+      expect(req_json.class).to eql(String)
+    end
+
+    it 'should return an array response is not empty' do
+      expect(req_json.length).not_to eql(0)
+    end
+  end
+end 
